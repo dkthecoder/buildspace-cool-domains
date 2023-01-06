48c4e206bb79dd0b8c9bcdc35e613b827f6411c9
Dee Kay <dk@email.com>
2023-01-06 15:53:46 +0000

    fixed price function

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -33,19 +33,6 @@ contract Domains is ERC721URIStorage {
         console.log("%s name service deployed", _tld);
     }
 
-    //price based on length function
-    function price(string calldata name) public pure returns (unit) {
-        uint256 len = StringUtils.strlen(name);
-        require(len > 0);
-        if (len == 3) {
-            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
-        } else if (len == 4) {
-            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
-        } else {
-            return 1 * 10**17;
-        }
-    }
-
     //register function
     function register(string calldata name) public payable {
         require(domains[name] == address(0));
@@ -83,6 +70,19 @@ contract Domains is ERC721URIStorage {
         domains[name] = msg.sender;
 
         _tokenIds.increment();
+    }
+
+    //price based on length function
+    function price(string calldata name) public pure returns(uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+            return 1 * 10**17;
+        }
     }
 
     //domain owners address
