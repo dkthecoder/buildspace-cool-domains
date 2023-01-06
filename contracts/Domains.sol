61b27be883317f7dd4bfa4edde880ebf38d3d8ae
Dee Kay <dk@email.com>
2023-01-06 12:35:58 +0000

    test to set non-owner domain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,9 +5,35 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    //mapping for domains
+    mapping(string => address) public domains;
+    //mapping for records
+    mapping(string => address) public records;
+
     constructor(){
-        console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
+        console.log("contract... nice");
     }
 
+    //register function
+    function register(string calldata name) public {
+        //check if name exists
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
 
+    //domain owners address
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        //check owner is transaction sender
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory){
+        return records[name];
+    }
 }
