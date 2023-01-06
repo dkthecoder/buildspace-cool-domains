0018da5b0f62d3356e45c5b79687aad8b1df9a81
Dee Kay <dk@email.com>
2023-01-06 12:43:29 +0000

    fixed test

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -8,7 +8,7 @@ contract Domains {
     //mapping for domains
     mapping(string => address) public domains;
     //mapping for records
-    mapping(string => address) public records;
+    mapping(string => string) public records;
 
     constructor(){
         console.log("contract... nice");
@@ -16,24 +16,24 @@ contract Domains {
 
     //register function
     function register(string calldata name) public {
-        //check if name exists
-        require(domains[name] == address(0));
-        domains[name] = msg.sender;
-        console.log("%s has registered a domain!", msg.sender);
-    }
+      // Check if name exists
+      require(domains[name] == address(0));
+      domains[name] = msg.sender;
+      console.log("%s has registered a domain!", msg.sender);
+  }
 
     //domain owners address
     function getAddress(string calldata name) public view returns (address) {
-        return domains[name];
-    }
+      return domains[name];
+  }
 
     function setRecord(string calldata name, string calldata record) public {
-        //check owner is transaction sender
-        require(domains[name] == msg.sender);
-        records[name] = record;
-    }
+      // check owner is transaction owner
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
 
-    function getRecord(string calldata name) public view returns(string memory){
-        return records[name];
-    }
+    function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
+  }
 }
